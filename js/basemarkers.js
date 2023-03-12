ba9a95e65480003cdc5eee89ad826a7580d3a56d
sufianstek <31790084+sufianstek@users.noreply.github.com>
2023-03-12 09:00:53 +0800

    Create basemarkers.js

---
+++ js/basemarkers.js
@@ -0,0 +1,32 @@
+var bases = 
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "base": "Hospital Kuala Lumpur"
+      },
+      "geometry": {
+        "coordinates": [
+          101.70132905919235,
+          3.17205885326365
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "base": "Hospital Ampang"
+      },
+      "geometry": {
+        "coordinates": [
+          101.76406202019405,
+          3.1271207809529926
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
